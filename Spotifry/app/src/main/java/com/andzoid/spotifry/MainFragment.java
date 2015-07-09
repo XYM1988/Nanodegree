18240919413f2603dbbad366aea7d7f6f1444cae
banging <mftznow@gmail.com>
2015-07-08 21:01:18 -0400

    P1
    Take 1

---
+++ Spotifry/app/src/main/java/com/andzoid/spotifry/MainFragment.java
@@ -0,0 +1,310 @@
+package com.andzoid.spotifry;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.Tracks;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MainFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+//
+//    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MainFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+//    public static MainFragment newInstance(String param1, String param2) {
+//        MainFragment fragment = new MainFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    private ArtistAdapter artistAdapter;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        this.setRetainInstance(true);
+
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        artistAdapter = new ArtistAdapter(getActivity(), new ArrayList<Artist>());
+
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) v.findViewById(R.id.listView);
+        listView.setAdapter(artistAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Artist a = artistAdapter.getItem(position);
+
+                SpotifyArtistTopTenTracksAsyncTask task = new SpotifyArtistTopTenTracksAsyncTask();
+                task.execute(a.name);
+
+
+            }
+        });
+
+        EditText editText = (EditText) v.findViewById(R.id.editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                    SpotifyArtistSearchAsyncTask task = new SpotifyArtistSearchAsyncTask();
+                    task.execute(v.getText().toString());
+
+                    //Toast.makeText(getActivity(), v.getText(), Toast.LENGTH_SHORT).show();
+
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction(Uri uri);
+//    }
+
+
+
+
+
+
+    public class SpotifyArtistSearchAsyncTask extends AsyncTask<String, Void, List<Artist>>
+    {
+        @Override
+        protected List<Artist> doInBackground(String... sName) {
+
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService service = api.getService();
+
+            ArtistsPager results = service.searchArtists(sName[0]);
+
+            List<Artist> artists = results.artists.items;
+
+            return artists;
+        }
+
+        @Override
+        protected void onPostExecute(List<Artist> l) {
+
+            if (l != null) {
+
+                if (l.isEmpty()) {
+
+                    Toast.makeText(getActivity(), getString(R.string.artist_not_found_msg), Toast.LENGTH_SHORT).show();
+                }
+
+                artistAdapter.clear();
+
+                for (Artist a : l) {
+
+                    artistAdapter.add(a);
+                }
+            }
+        }
+    }
+
+    public class SpotifyArtistTopTenTracksAsyncTask extends AsyncTask<String, Void, List<Track>>
+    {
+        @Override
+        protected List<Track> doInBackground(String... sName) {
+
+            SpotifyApi api = new SpotifyApi();
+            SpotifyService service = api.getService();
+
+            Tracks results = service.getArtistTopTrack(sName[0]);
+
+            List<Track> tracks = results.tracks;
+
+            return tracks;
+        }
+
+        @Override
+        protected void onPostExecute(List<Track> l) {
+
+            if (l != null) {
+
+                if (l.isEmpty()) {
+
+                    Toast.makeText(getActivity(), getString(R.string.artist_tracks_not_found_msg), Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    Intent intent = new Intent(getActivity(), TracksActivity.class).putExtra("Tracks", (Serializable) l);
+                    startActivity(intent);
+                }
+            }
+        }
+    }
+
+
+
+    public class ArtistAdapter extends ArrayAdapter<Artist> {
+        //private static final String LOG_TAG = AndroidFlavorAdapter.class.getSimpleName();
+
+        /**
+         * This is our own custom constructor (it doesn't mirror a superclass constructor).
+         * The context is used to inflate the layout file, and the List is the data we want
+         * to populate into the lists
+         *
+         * @param context        The current context. Used to inflate the layout file.
+         * @param l A List of AndroidFlavor objects to display in a list
+         */
+        public ArtistAdapter(Activity context, List<Artist> l) {
+            // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+            // the second argument is used when the ArrayAdapter is populating a single TextView.
+            // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+            // going to use this second argument, so it can be any value. Here, we used 0.
+            super(context, 0, l);
+        }
+
+        /**
+         * Provides a view for an AdapterView (ListView, GridView, etc.)
+         *
+         * @param position    The AdapterView position that is requesting a view
+         * @param convertView The recycled view to populate.
+         *                    (search online for "android view recycling" to learn more)
+         * @param parent The parent ViewGroup that is used for inflation.
+         * @return The View for the position in the AdapterView.
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Gets the AndroidFlavor object from the ArrayAdapter at the appropriate position
+            Artist a = getItem(position);
+
+            // Adapters recycle views to AdapterViews.
+            // If this is a new View object we're getting, then inflate the layout.
+            // If not, this view already has the layout inflated from a previous call to getView,
+            // and we modify the View widgets as usual.
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.listviewitem_artist, parent, false);
+            }
+
+            ImageView iv = (ImageView) convertView.findViewById(R.id.artist_icon);
+
+            if (!a.images.isEmpty()) {
+                Picasso.with(getContext()).load(a.images.get(0).url).into(iv);
+            }
+
+            TextView tv = (TextView) convertView.findViewById(R.id.artist_name);
+            tv.setText(a.name);
+
+
+            return convertView;
+        }
+    }
+
+
+}
